c575ad41256e32d2bad5770c469bffd5117b8308
Jamaillah022306 <j.santi.554449@umindanao.edu.ph>
2026-02-23 13:51:22 +0800

    first commit

---
+++ nav.php
@@ -0,0 +1,10 @@
+<?php // nav.php ?>
+<div style="display:flex; gap:10px; flex-wrap:wrap; margin-bottom:16px;">
+  <a href="/santi_assessment/index.php">Dashboard</a>
+  <a href="/santi_assessment/pages/clients_list.php">Clients</a>
+  <a href="/santi_assessment/pages/services_list.php">Services</a>
+  <a href="/santi_assessment/pages/bookings_list.php">Bookings</a>
+  <a href="/santi_assessment/pages/tools_list_assign.php">Tools</a>
+  <a href="/santi_assessment/pages/payments_list.php">Payments</a>
+</div>
+<hr>
